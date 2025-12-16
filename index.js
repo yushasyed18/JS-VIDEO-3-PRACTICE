1d52899f8fb0af08d01163a9e5bb68d37070d32e
yushasyed18 <yushasyed18@gmail.com>
2025-12-16 17:10:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,156 @@
+var a = 2
+// var decre = a--
+// console.log(a--)
+// // console.log(decre)
+
+// var decre = a-- - a--
+// var decre = a-- - a++ + a--
+// console.log(decre)
+
+// var a = 1
+// var b = 1
+
+// var result = ++a + b++ + a++ + ++a + b++    
+// console.log(result)
+
+
+// Math Operations
+
+// var num = ((12 + 5) * 9) / 2
+// console.log(num)
+
+// var myMessage = "Thank you "
+// var uname = "Yusha "
+// console.log(myMessage + uname)
+
+// var message = "Thanks" + "! "
+// var uname = "Yusha " 
+// alert(message + uname)
+// console.log(message + uname + "Muhammad")
+
+// PROMPT: MESSAGE WITH USER INFORMATION 
+// prompt("edit your name" , "Yusha")
+
+// var userName = "Yusha"
+// var message = "AssalamuAliekum"
+// var result = prompt(userName , message)
+
+// var number1 = Number(prompt("enter first number"));
+// var number2 = +(prompt("enter second number"));
+
+// var result = number1 + number2
+// console.log(result)
+
+// var cats = prompt("how many cats in your area?")
+// var calculate = cats + 1000 
+// console.log("calculate=>",calculate)
+
+// var prompt = require('prompt-sync')();
+
+// var prompt = +prompt("enter a number")
+// var sum = prompt + 2
+// console.log("prompt=>", sum)
+
+// if else statements
+
+// if (true) {
+//     console.log("cond is true")
+// }
+// console.log("outside cond")
+
+// if (false) {
+//     console.log("cond is true")
+// }
+// console.log("outside cond")
+
+// if (1) {
+//     console.log("cond is true")
+// }
+// console.log("outside cond")
+
+// if (0) {
+//     console.log("cond is true")
+// }
+// console.log("outside cond")
+
+// 1 = true & 0 = false 
+ 
+// var uname = prompt("enter ur name")
+// var absentStudent = prompt("enter absent student")
+
+// 3 === ka kya matlab? 
+// = ka matlab kisi value ke andar value assign krna, == ka matlab hn ke us value dono ko check krna ke equal hn ke nahi, === ka matlab yeh hn ke us value ko bhi check krna or uski kya type hn number ya string.
+
+// if (uname === absentStudent) {
+//     console.log("you are not allowed")
+// }
+
+// if (uname == absentStudent) {
+//     console.log("you are not allowed")
+// }
+
+// if (uname = absentStudent) {
+//     console.log("you are not allowed")
+// }
+
+// if (uname === absentStudent) {
+//     console.log(uname + " you are not allowed")
+// }
+
+// else {
+//     console.log(uname +" you are allow")
+// }
+
+// comparision 
+
+// var num1 = 2
+// var num2 = 3
+
+// if (num2 >= num1) {
+//     console.log("you are right")
+// }
+
+// ! Not Equal to ka sign hota hn 
+
+// if (num1 !== num2) {
+//     console.log(num1 +" not equal to " + num2)
+// }
+
+// var uname = prompt("enter user name")
+// // var absentName = "ali"
+// // var absentName = "asad"
+
+// if (uname === "ali") {
+//     console.log("You were absent ")
+// }
+
+// else if (uname === "asad") {
+//     console.log("You were absent ")
+// }
+
+// else if (uname === "bilal") {
+//     console.log("You were absent ")
+// }
+
+// else {
+//     console.log("You were present")
+// }
+
+
+// var stdName = prompt("enter your name")
+// var stdAge = Number(prompt("enter your age"))
+
+// var name = "ali"
+// var age = 15
+
+// // && operator use hota hn agar dono chalane hu 
+
+// // if(stdName === name && stdAge === age){
+// //     console.log("You passed Matric")
+// // }
+
+// // || (OR operator) use hota hn agar koi aik 
+
+// if(stdName === name || stdAge === age){
+//     console.log("You passed Matric")
+// }
